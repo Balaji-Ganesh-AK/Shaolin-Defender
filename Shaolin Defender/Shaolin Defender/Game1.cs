e095291c4cbe45766fc83bd09405aabdcff4f323
Darkninija <balaji.balaji.ganesh@gmail.com>
2019-08-23 17:35:38 -0600

    Update Game1.cs

--- Shaolin Defender/Shaolin Defender/Game1.cs
+++ Shaolin Defender/Shaolin Defender/Game1.cs
@@ -55,22 +55,17 @@ namespace Shaolin_Defender
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            // testing 
-            //player = new Texture2D(this.GraphicsDevice, 100 , 100);
-            //Color[] colorData = new Color[100*100];
-            //for (int i = 0; i < 10000; i++)
-            //    colorData[i] = Color.Black;
-            //player.SetData<Color>(colorData); // color of the cube ; 
-            // playerPos.X = (Window.ClientBounds.Width - player.Width) / 2;
-            // playerPos.Y = (Window.ClientBounds.Height - player.Height);
-            //  widthWindow = GraphicsDevice.Viewport.Width;
-            //testing 
-            // heigthWindow = GraphicsDevice.Viewport.Height;
-            graphics.PreferredBackBufferWidth = 820;
-             graphics.PreferredBackBufferHeight = 1010;
-            graphics.IsFullScreen = false;
+            // Window Size Controls
+            widthWindow = GraphicsDevice.Viewport.Width;
+            heigthWindow = GraphicsDevice.Viewport.Height;
+               graphics.PreferredBackBufferWidth = 1500;
+               graphics.PreferredBackBufferHeight = 1010;
+                
+               graphics.IsFullScreen = false;
            
             graphics.ApplyChanges();
+           
+
             coinPos = new Vector2(50, 50);
             base.Initialize();
 
@@ -85,7 +80,8 @@ namespace Shaolin_Defender
             // Create a new SpriteBatch, which can be used to draw players.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             // testing 
-            player = this.Content.Load<Texture2D>("RedBoi");
+            //Loading Texture
+            player = this.Content.Load<Texture2D>("Coin");
             coins = this.Content.Load<Texture2D>("Coin");
             scoreFont = Content.Load<SpriteFont>("Title");
 
@@ -108,6 +104,7 @@ namespace Shaolin_Defender
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+           
             playerRectangle = new Rectangle((int)(playerPos.X), (int)(playerPos.Y), player.Width, player.Height);
             //TODO : add a for loop to go thro all the vectors of coins;
             coinsRectangle = new Rectangle((int)(coinPos.X), (int)(coinPos.Y), coins.Width, coins.Height);
@@ -116,18 +113,23 @@ namespace Shaolin_Defender
             isHit = false;
             // widthWindow = GraphicsDevice.Viewport.Width;
             //   heigthWindow = GraphicsDevice.Viewport.Height;
-
+            
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            //claming player movements
+            playerPos.X = MathHelper.Clamp(playerPos.X,0, 1190-player.Width);
+         //   playerPos.X = MathHelper.Clamp(0,playerPos.Y, Window.ClientBounds.Height - player.Height);
+
             //Keymovements
 
-            if (Keyboard.GetState().IsKeyDown(Keys.W) && playerPos.Y >= 10)
+            if (Keyboard.GetState().IsKeyDown(Keys.W) && playerPos.Y>0 + player.Height/2)
                 playerPos.Y -= speed;
-            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            if (Keyboard.GetState().IsKeyDown(Keys.S) && playerPos.Y < 390)
                 playerPos.Y += speed;
-            if (Keyboard.GetState().IsKeyDown(Keys.A) && playerPos.X >= 0)
+            if (Keyboard.GetState().IsKeyDown(Keys.A) && playerPos.X>0+player.Width/2)
                 playerPos.X -= speed;
-            if (Keyboard.GetState().IsKeyDown(Keys.D) )
+            if (Keyboard.GetState().IsKeyDown(Keys.D) && playerPos.X < 390)
                 playerPos.X += speed;
             if (Keyboard.GetState().IsKeyDown(Keys.Q))
                 angle -= 0.1f;
@@ -154,7 +156,7 @@ namespace Shaolin_Defender
             Rectangle sourceRectangle = new Rectangle(0, 0, player.Width, player.Height);
             Vector2 origin = new Vector2(player.Width / 2, player.Height / 2);
             spriteBatch.Draw(player, playerPos * 2, sourceRectangle, Color.White, angle, origin, 1.0f, SpriteEffects.None, 1);
-            spriteBatch.Draw(coins, coinPos, null, Color.White, 0f, Vector2.Zero, 0.1f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(coins, coinPos, Color.White);
             //  spriteBatch.Draw(player, position:playerPos);
             //text
             //spriteBatch.DrawString(scoreFont, "rectanglearrowright2" + arrow_collision_detection_2.Right, new Vector2(150, 150), Color.Black);// width
@@ -166,6 +168,9 @@ namespace Shaolin_Defender
             //spriteBatch.DrawString(scoreFont, " rectanglearrowrbottom1" + arrow_collision_detection_1.Bottom, new Vector2(550, 450), Color.Black);
             //spriteBatch.DrawString(scoreFont, " rectanglearrowrTop1" + arrow_collision_detection_1.Top, new Vector2(550, 550), Color.Black);
             //spriteBatch.DrawString(scoreFont, "arrow2 x" + playerPos_coins.X, new Vector2(600, 300), Color.Black);//height
+            spriteBatch.DrawString(scoreFont, "x , y " + playerPos.X + " " + playerPos.Y, new Vector2(400, 400), Color.Black);
+            spriteBatch.DrawString(scoreFont, "window(x , y )" + Window.ClientBounds.Width + " " + Window.ClientBounds.Height, new Vector2(500, 400), Color.Black);
+
             if (isHit == true)
             {
                 spriteBatch.DrawString(scoreFont, "Collision detected", new Vector2(700, 70), Color.Black);
